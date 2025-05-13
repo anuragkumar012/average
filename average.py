3c45e357c190964fc3ab663f1fab62ccead60d56
Anurag Kumar <anuragkumar8824ak@gmail.com>
2025-05-13 15:24:02 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,7 @@
+def average():
+    n=int(input("enter the number:"))
+    a=int(input("enter the number:"))
+    b=int(input("enter the number"))
+    average = (n+a+b)/3
+    return average
+print('average:',average())
